826bbeb7553f3e8daf826d20d6d73e39061eba99
JOSH ALEISTER VALENZUELA <joshaleisterv@gmail.com>
2021-10-24 21:10:32 +0800

    inconsistency between stock increase and stock decrease

--- PoSExpress/ProductManagementModule/Modules/ProductStockIncrease.cs
+++ PoSExpress/ProductManagementModule/Modules/ProductStockIncrease.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PoS.ProductModels;
 using PoS.LocalValidator;
 namespace PoS.ProductManagementModule
 {
-    public class ProductStockIncrease
+    public class ProductStockIncrease : GeneralInput
     {
         public ProductStockIncrease(PoSInventory PoSInventoryRepository)
         {
             StockInput AddendEncoder = new StockInput();
             string productCategory = GeneralInput.GetProductCategory();
             string productName = GeneralInput.GetProductName(productCategory, PoSInventoryRepository);
+            ProductModel product = PoSInventoryRepository.GetProduct(productCategory, productName);
             uint increment = AddendEncoder.GetAddend();
 
             PoSInventoryRepository.STOREInventory.IncreaseStock(productCategory, productName, increment);
+            PoSInventoryRepository.UpdateInventory(productCategory);
         }
     }
 }
